0d2d0c35491f3f96826d7d3104230b3c9c2ccf36
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2021-12-16 20:05:47 -0600

    Initial Commit

---
+++ procfs_fuse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  procfs_fuse
+//
+//  Created by Sudhip Nashi on 12/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
